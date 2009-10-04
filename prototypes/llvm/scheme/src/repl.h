2f7d5c3ed1acefcad1e5e52adc22389f4b850853
Jonathan Tang <jdtang@jonathan-tangs-macbook-pro.local>
2009-10-04 12:07:47 -0700

    Added (unimplemented) classes for the various types of Repl lines.

--- prototypes/llvm/scheme/src/repl.h
+++ prototypes/llvm/scheme/src/repl.h
@@ -31,6 +31,29 @@ class Repl {
   llvm::ExecutionEngine* jit_;
 };
 
+class ReplLine {
+  virtual std::string Eval(Repl* repl) = 0;
+};
+
+class ReplExpr : public ReplLine {
+ public:
+  ReplExpr(eve::expr::Expr* expr) : expr_(expr) {}
+  virtual std::string Eval(Repl* repl);
+
+ private:
+  boost::scoped_ptr<eve::expr::Expr> expr_;
+};
+
+class ReplAssignment : public ReplLine {
+ public:
+  ReplAssignment(const std::string& var, ReplExpr* expr)
+      : var_(var), expr_(expr) {}
+  virtual std::string Eval(Repl* repl);
+ private:
+  std::string var_;
+  boost::scoped_ptr<ReplExpr> expr_;
+};
+
 } // namespace eve
 
 #endif EVE_REPL_H
