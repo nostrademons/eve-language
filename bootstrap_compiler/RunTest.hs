d9db6f1e4f981c1d67b75b8300c9b48b25da8682
Jonathan Tang <jdtang@new-host.home>
2009-12-22 14:56:06 -0800

    Pandoc-parse tests; not quite working yet.

--- bootstrap_compiler/RunTest.hs
+++ bootstrap_compiler/RunTest.hs
@@ -1,5 +1,6 @@
 module Main(main) where
 import Data.List
+import IO
 import Monad
 import Text.Pandoc
 
@@ -10,6 +11,19 @@ filterExtensions ext = notHas ".svn" . notHas "~" . notHas ".swp" . hasExt ext
     notHas ext = filter (not . isSuffixOf ext)
     hasExt ext = filter (isSuffixOf ("." ++ ext))
 
+extractCode :: FilePath -> IO String
+extractCode filename = do
+    file <- openFile filename ReadMode
+    contents <- hGetContents file
+    hClose file
+    pandoc <- return $ readMarkdown defaultParserState contents
+    return $ concat $ codeBlocks pandoc
+  where
+    codeBlocks (Pandoc _ blocks) = map codeBlockText blocks
+    codeBlockText (CodeBlock _ text) = text
+    codeBlockText _ = ""
+
 main = do
-  files <- liftM (filterExtensions "evetest") $ dirWalk "../test"
-  putStrLn $ concatMap (++ "\n") files
+    files <- liftM (filterExtensions "evetest") $ dirWalk "../test"
+    text <- mapM extractCode files
+    putStrLn $ concatMap (++ "\n") text
