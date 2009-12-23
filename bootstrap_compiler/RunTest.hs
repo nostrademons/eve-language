c69ed4d7cdbd186b0a78e4abad84d113c263bf50
Jonathan Tang <jdtang@new-host.home>
2009-12-22 21:36:38 -0800

    Can generate an Eve program from the doctests now.  Next: have Eve compiler output the LLVM bitcode of evaluating one of these expressions.

--- bootstrap_compiler/RunTest.hs
+++ bootstrap_compiler/RunTest.hs
@@ -6,23 +6,52 @@ import Text.Pandoc
 
 import Utils
 
+data Fixture = Fixture String
+data Expect = Expect String String
+data CodeChunk =
+    CodeTest [Expect]
+  | CodeChunk Fixture
+
 filterExtensions ext = notHas ".svn" . notHas "~" . notHas ".swp" . hasExt ext
   where
     notHas ext = filter (not . isSuffixOf ext)
     hasExt ext = filter (isSuffixOf ("." ++ ext))
 
-extractCode :: FilePath -> IO String
-extractCode filename = do
-    file <- openFile "/Users/jdtang/Documents/programming/eve/eve-language/test/number.evetest" ReadMode
-    contents <- hGetContents file
-    pandoc <- return $ readMarkdown defaultParserState contents
-    return $ concat $ codeBlocks pandoc
+parseCode :: String -> [CodeChunk]
+parseCode = map parseBlock . codeBlocks . readMarkdown defaultParserState
   where
-    codeBlocks (Pandoc _ blocks) = map codeBlockText blocks
-    codeBlockText (CodeBlock _ text) = text ++ "\n"
-    codeBlockText _ = ""
+    codeBlocks (Pandoc _ blocks) = filter isCodeBlock blocks
+    isCodeBlock (CodeBlock _ _) = True
+    isCodeBlock _ = False
+    isDocTest = isPrefixOf ">>> "
+    parseDocTests (line:lines) =
+        Expect cleanedInput (if null output then "" else last output) :
+            parseDocTests remainder
+      where
+        cleanedInput = drop (length ">>> ") line
+        (output, remainder) = break isDocTest lines
+    parseDocTests [] = []
+    parseBlock (CodeBlock _ text) =
+        if isDocTest text
+          then CodeTest (parseDocTests $ lines text)
+          else CodeChunk (Fixture text)
+
+generateCode :: [CodeChunk] -> String
+generateCode chunks = concatMap generateCodeFor chunks
+  where
+    generateCodeFor (CodeChunk (Fixture text)) = text
+    generateCodeFor (CodeTest tests) = concatMap generateTestCode tests
+    generateTestCode (Expect input _) =
+        "print(\"" ++ input ++ "\")\nprint(" ++ input ++ ")\n"
+
+outputCode :: FilePath -> IO String
+outputCode filename = do
+    file <- openFile filename ReadMode
+    contents <- hGetContents file
+    return . generateCode . parseCode $ contents
+  where
 
 main = do
     files <- liftM (filterExtensions "evetest") $ dirWalk "../test"
-    text <- mapM extractCode files
+    text <- mapM outputCode files
     putStrLn $ concatMap (++ "\n") text
