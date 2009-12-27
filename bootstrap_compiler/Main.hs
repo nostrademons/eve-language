107a95fe967d80e3332f07e5f119106fd512da44
Jonathan Tang <jdtang@jonathan-tangs-macbook-pro.local>
2009-12-27 13:36:16 -0800

    Moved Repl code to Main.hs

--- bootstrap_compiler/Main.hs
+++ bootstrap_compiler/Main.hs
@@ -1,15 +1,41 @@
 module Main(main) where
 import System
 import IO
+import List
 import Maybe
+import Monad
 import Control.Monad.Error
+import Control.Monad.Trans
 
 import Expr (FileLineValue(..), exprType, fileLineVal)
 import Lexer
 import Parser
 import TypeCheck
 import Error
-import Repl
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: MonadIO m => Bool -> String -> m String
+readPrompt isContinuation prompt = liftIO $ do
+    flushStr (if isContinuation then replicate (length prompt)'.' ++ "... " 
+                                 else '\n' : prompt ++ ">>> ")
+    line <- getLine
+    if "\\" `isSuffixOf` line
+        then liftM ((init line ++ "\n") ++) $ readPrompt True prompt
+        else return line
+
+until_ :: MonadIO m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do 
+    result <- prompt
+    if pred result 
+        then return ()
+        else action result >> until_ pred prompt action
+
+replOutput output = liftIO . putStr . show $ output
+
+runRepl :: MonadIO m => String -> (String -> m ()) -> m ()
+runRepl prompt action = until_ (== "quit") (readPrompt False prompt) action
 
 parseLine input = putStrLn output
   where
--- bootstrap_compiler/Repl.hs
+++ bootstrap_compiler/Repl.hs
@@ -3,27 +3,3 @@ import IO
 import List
 import Monad
 import Control.Monad.Trans
-
-flushStr :: String -> IO ()
-flushStr str = putStr str >> hFlush stdout
-
-readPrompt :: MonadIO m => Bool -> String -> m String
-readPrompt isContinuation prompt = liftIO $ do
-    flushStr (if isContinuation then replicate (length prompt)'.' ++ "... " 
-                                 else '\n' : prompt ++ ">>> ")
-    line <- getLine
-    if "\\" `isSuffixOf` line
-        then liftM ((init line ++ "\n") ++) $ readPrompt True prompt
-        else return line
-
-until_ :: MonadIO m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
-until_ pred prompt action = do 
-    result <- prompt
-    if pred result 
-        then return ()
-        else action result >> until_ pred prompt action
-
-replOutput output = liftIO . putStr . show $ output
-
-runRepl :: MonadIO m => String -> (String -> m ()) -> m ()
-runRepl prompt action = until_ (== "quit") (readPrompt False prompt) action
