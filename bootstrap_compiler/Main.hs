fe3f3b490b802b34192b870234e52fa5f1879699
Jonathan Tang <jdtang@jonathan-tangs-macbook-pro.local>
2010-01-01 18:41:18 -0800

    Created skeleton codegen for Eve compiler.

--- bootstrap_compiler/Main.hs
+++ bootstrap_compiler/Main.hs
@@ -1,12 +1,39 @@
 module Main(main) where
+import Data.Word
 import IO
 import System
+import System.Cmd
+import LLVM.Core
+
+compilePlaceholder :: CodeGenModule ()
+compilePlaceholder = do
+  printf <- newNamedFunction ExternalLinkage "printf"
+      :: TFunction (Ptr Word8 -> IO Word32)
+  msg <- createStringNul "Hello, Eve!\n"
+  main <- newNamedFunction ExternalLinkage "main"
+      :: TFunction (Word32 -> Ptr (Ptr Word8) -> IO Word32)
+  defineFunction main $ \argc argv -> do
+    tmp <- getElementPtr0 msg (0::Word32, ())
+    call printf tmp
+    ret (1::Word32)
+
+writeCompiledFile moduleName = do
+  mod <- newNamedModule moduleName
+  defineModule mod compilePlaceholder
+  let bcFile = moduleName ++ ".bc"
+  let sFile = moduleName ++ ".s"
+  writeBitcodeToFile bcFile mod
+  rawSystem "llc" [bcFile]
+  rawSystem "gcc" [sFile, "-o", moduleName]
 
 main = do
   args <- getArgs
   if null args
     then putStrLn "Usage: eve <file to compile>"
     else do
-      inFile <- openFile (args !! 0) ReadMode
+      let filename = args !! 0
+      inFile <- openFile (filename ++ ".evetest") ReadMode
       text <- hGetContents inFile
       putStrLn text
+      writeCompiledFile filename
+      return ()
